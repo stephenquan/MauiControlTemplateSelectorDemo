7d17fbc52687b332c5f20d1cb05b803e3a1539a8
Stephen Quan <squan@esri.com>
2025-03-03 14:29:56 +1100

    Add documentation for ResourceContentView

--- MauiControlTemplateSelectorDemo/ResourceContentView.cs
+++ MauiControlTemplateSelectorDemo/ResourceContentView.cs
@@ -1,24 +1,47 @@
 ﻿namespace MauiControlTemplateSelectorDemo;
 
+/// <summary>
+/// A ContentView that displays a ControlTemplate from a ResourceDictionary.
+/// </summary>
 public partial class ResourceContentView : ContentView
 {
+	/// <summary>
+	/// Bindable property for <see cref="Key"/>.
+	/// </summary>
 	public static BindableProperty KeyProperty = BindableProperty.Create(nameof(Key), typeof(string), typeof(ResourceContentView), null,
 		propertyChanged: (b, o, n) => ((ResourceContentView)b).RefreshControlTemplate());
+
+	/// <summary>
+	/// The key of the ControlTemplate to display.
+	/// </summary>
 	public string Key
 	{
 		get => (string)GetValue(KeyProperty);
 		set => SetValue(KeyProperty, value);
 	}
 
+	/// <summary>
+	/// Bindable property for <see cref="Source"/>.
+	/// </summary>
 	public static BindableProperty SourceProperty = BindableProperty.Create(nameof(Source), typeof(ResourceDictionary), typeof(ResourceContentView), null);
+
+	/// <summary>
+	/// The ResourceDictionary to look up the ControlTemplate in.
+	/// </summary>
 	public ResourceDictionary Source
 	{
 		get => (ResourceDictionary)GetValue(SourceProperty);
 		set => SetValue(SourceProperty, value);
 	}
 
+	/// <summary>
+	/// A default ControlTemplate to display if the key is not found in the Source.
+	/// </summary>
 	public ControlTemplate? Default { get; set; }
 
+	/// <summary>
+	/// Internal method to refresh the ControlTemplate.
+	/// </summary>
 	void RefreshControlTemplate()
 	{
 		ControlTemplate = (Key is string key
@@ -27,6 +50,9 @@ public partial class ResourceContentView : ContentView
 			&& template is ControlTemplate ct) ? ct : Default;
 	}
 
+	/// <summary>
+	/// Initializes a new instance of <see cref="ResourceContentView"/>.
+	/// </summary>
 	public ResourceContentView()
 	{
 		RefreshControlTemplate();
